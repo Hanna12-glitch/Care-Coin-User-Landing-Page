89c2ddcde9a4e38d6f85a0623ff3377a61ae9138
Idontknow <64530931+Hanna12-glitch@users.noreply.github.com>
2026-04-20 11:26:06 +0000

    Button

--- projects/TokenizeRWATemplate-frontend/src/Dashboard.tsx
+++ projects/TokenizeRWATemplate-frontend/src/Dashboard.tsx
@@ -346,7 +346,7 @@ export default function Dashboard() {
             href="mailto:help@project-care-coin.org"
             className="inline-block py-3 px-8 rounded-2xl border border-white/20 bg-blue/5 hover:bg-[#fa1179] text-white font-bold text-sm transition-all"
           >
-            contact
+            HELP
           </a>
         </div>
 
